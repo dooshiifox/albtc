69fd54bab2f1ba479c160cc2937d32c7eca47607
kit <edoosh.yt@gmail.com>
2024-04-10 11:47:43 +1200

    Breaking changes to `tryString`

--- src/safeParse.ts
+++ src/safeParse.ts
@@ -42,8 +42,18 @@ export function safeParseFloat(x: unknown): number | null {
 	return number;
 }
 
-/** Tries to convert a value into a string. */
-export function tryString(thing: unknown) {
+/** Tries to convert a value into a string. Unlike `JSON.stringify`, this attempts
+ *  to convert the value into a string in something an end-user could read.
+ *  For example, it converts `Error` objects into their message and `Date` objects
+ *  into a local time string, among other things.
+ *
+ *  If a `fallback` function is provided, it will be called when the value is not
+ *  stringifiable by this function. The fallback function should return a string.
+ */
+export function tryString(
+	thing: unknown,
+	fallback?: (thing: object) => string
+) {
 	if (typeof thing === "string") return thing;
 	if (
 		typeof thing === "number" ||
@@ -52,18 +62,22 @@ export function tryString(thing: unknown) {
 	) {
 		return thing.toString();
 	}
-	if (thing === undefined) return "undefined";
-	if (thing === null) return "null";
+	if (thing === undefined) return "";
+	if (thing === null) return "";
 
 	if (thing instanceof Error) {
 		return thing.message;
 	}
 	if (thing instanceof Date) {
-		return thing.toISOString();
+		return thing.toLocaleString();
 	}
 	if (thing instanceof RegExp) {
 		return thing.toString();
 	}
+	if (thing instanceof Set) {
+		return `Set { ${Array.from(thing).join(", ")} }`;
+	}
 
+	if (fallback !== undefined) return fallback(thing);
 	return JSON.stringify(thing);
 }
